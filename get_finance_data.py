2b9d4172cda1f609376b3d9dceef2d33c869ee7b
renshiqiang <1140623243@qq.com>
2020-11-06 09:42:21 +0800

    Update get_finance_data.py

--- get_finance_data.py
+++ get_finance_data.py
@@ -2,15 +2,17 @@ import yfinance as yf
 import backtrader as bt
 import json
 from urllib.request import urlopen
+import pandas as pd
+import csv
+import os
 
-
-def get_data(stock_name='AAPL', start='2015-01-01', end='2015-12-25'):
+def get_data(stock_name='GE', start='2015-01-01', end='2015-12-25', date='2y'):
     stock_names = stock_name.split()
-    # df = yf.download(stock_name, start=start, end=end, group_by='ticker')
-    try:
-        df = yf.Ticker(stock_name).history(period='5y')
-    except:
-        pass
+    df = yf.Ticker(stock_name).history(period=date)
+    # try:
+    #     df = yf.download(stock_name, start=start, end=end, group_by='ticker')
+    # except:
+    #     df = yf.Ticker(stock_name).history(period=date)
 
     return df, stock_names
 
@@ -25,4 +27,28 @@ def get_jsonparsed_data(url):
     return company_info
 
 
+def download_stock_data(stock_name='AAPL', start='2015-01-01', end='2016-12-25', date='2y'):
+    data, _ = get_data(stock_name=stock_name, start=start, end=end, date=date)
+    main_path = 'D:\\pycharmprojects\\backtrader-general-api\\data\\stocks\\'
+    path = main_path + stock_name + '.csv'
+    data.to_csv(path)
 
+
+def from_csv_read_company_name(file):
+    company_name = []
+    with open(file) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            company_name.append(row[0])
+
+    return company_name
+
+
+def from_local_read_company_name():
+    filepath = 'D:\\pycharmprojects\\backtrader-general-api\\data\\stocks\\'
+    filenames = os.listdir(filepath)
+    company_names = []
+    for filename in filenames:
+        company = filename.split('.')[0]
+        company_names.append(company)
+    return company_names
