1b9c4c0d4e42923d6d87b88983c132f7a146cbe9
bobo <jiangzhubo1992@outlook.com>
2021-05-13 19:04:23 +0800

    Update send_email.py.py

--- send_email.py.py
+++ send_email.py.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[117]:
+# In[172]:
 
 
 import smtplib
@@ -13,6 +13,7 @@ import os.path
 import numpy as np
 import glob
 import pandas as pd
+import datetime
 
 class Email:
     
@@ -59,86 +60,96 @@ class Email:
 
     
 def change_time_to_number(time):
+    # time = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
     df = pd.DataFrame({'time': [pd.to_datetime(time)]})
-    df_unix_sec = pd.to_datetime(df['time']).astype(int)/ 10**9
-    return list(df_unix_sec)[0]
+    df_unix_sec = pd.to_numeric(df['time']) [0]
+    return df_unix_sec
 
 
-def compose_email ( stock,
-                    price = '29',
-                    stop_loss_range = '26-28',
-                    stop_earning_range = '30-31',
-                    direction = 'buy',
-                    size = '1',
-                    position = '1',
-                    risk = '60%'
-                ):
+def compose_email(trading_action):
     s = ','
-    subject = direction +' '+ stock + ' at price: ' + str(price)
-    info = ('Price: ' + str(price) , ' \nTrading size: ' +  str(size), ' \nCurrent postion: ' + str(position),
-            ' \nStop_loss_price: ' + str(stop_loss_range),
-            ' \nStop_earning_range: ' + str(stop_earning_range),
-           
-           )
+    direction = trading_action['Direction']
+    price = round(trading_action['Price'],3)
+    stop_loss_range = round(trading_action['stop_loss_range'] ,3)
+    stop_earning_range = round(trading_action['stop_earning_range'] ,3)
+    subject = direction +' '+ trading_action['Symbol'] + ' at price: ' + str(price)
+    if direction == 'buy':
+        info = ('Price: ' + str(price) , ' \nTrading size: ' +  str(trading_action['Size']), ' \nCurrent postion: ' +  str(trading_action['Position']),
+                ' \nStop_loss_price: ' + str(stop_loss_range),
+                ' \nStop_earning_range: ' + str(stop_earning_range)
+               )
+    elif direction == 'sell':
+        info = ('Price: ' + str(price) , ' \nTrading size: ' +  str(trading_action['Size']),
+                ' \nCurrent postion: ' + str(trading_action['Position'])
+               )
     content = s.join(info)
     return subject, content
 
 
-# In[131]:
+# In[169]:
+
+
+a = round(0.92,2)
+
+
+# In[170]:
+
+
+a
+
+
+# In[143]:
 
 
 
 # define users and what stocks should be provided
-users = {'409980834@qq.com':'all', 
-         '973099449@qq.com':'all', 
-         '1033904749@qq.com':'all', 
-         '1140623243@qq.com':'all', 
-         'qiuansong@qq.com':'all',
-           'junelai999@gmail.com':'all', 
-         'fengyuxian1992@gmail.com':'all', 
-         'orangekate77@gmail.com':'all', 
-         'shaohua@gmail.com':'all'}
+users = {'409980834@qq.com':'all'}
+#          '973099449@qq.com':'all', 
+#          '1033904749@qq.com':'all', 
+#          '1140623243@qq.com':'all', 
+#          'qiuansong@qq.com':'all',
+#            'junelai999@gmail.com':'all', 
+#          'fengyuxian1992@gmail.com':'all', 
+#          'orangekate77@gmail.com':'all', 
+#          'shaohua@gmail.com':'all'}
       
 
 
-# In[133]:
+# In[ ]:
 
 
 
 recent_stock_update = {}
+start_time = change_time_to_number(datetime.date.today().strftime("%Y-%m-%d 09:30:00"))
 while True:
     for path in glob.glob('/home/zhubo/Documents/fda_check/test2/comparie/*.json'):
-        try:
+      #  try:
             stock_path = path
             stock = stock_path.split('/')[-1].split('.')[0]
             if stock not in list(recent_stock_update.keys()):
-                recent_stock_update[stock] = 0
+                recent_stock_update[stock] = start_time
             with open(stock_path) as f:       
                    data = json.load(f)
-
             time_key = [[change_time_to_number(time), time] for time in list(data.keys())]
             current_time = sorted(time_key , key = lambda x : x[0])[-1]
             if current_time[0] > recent_stock_update[stock]:
                 recent_stock_update[stock] = current_time[0]
                 trading_action = data[current_time[1]]
-                print(trading_action)
-                subject, content = compose_email( trading_action['Symbol'],
-                                price = trading_action['Price'],
-                                stop_loss_range = trading_action['stop_loss_range'],
-                                stop_earning_range = trading_action['stop_earning_range'],
-                                direction = trading_action['Direction'],
-                                size = trading_action['Size'],
-                                position = trading_action['Position'],
-                                risk = '60%')
-                print(subject,content)
+                subject, content = compose_email(trading_action)
                 for user in users.keys():
-                    print('send email to', user)
+                   
                     if users[user] == 'all':
-                        #subject, content =  compose_email()
                         e= Email()
                         e.send_email(user,
                                    subject,
                                    content, 
                                    '')
-        except Exception as e:
-            print(e)
+                    print('send email to', user)
+       # except Exception as e:
+        #    print(e)
+
+
+# In[ ]:
+
+
+
