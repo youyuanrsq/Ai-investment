f3d0f69acfcffcd125acf9254c8a528c814b9b1a
zhubo <jiangzhubo1992@outlook.com>
2021-07-04 14:03:06 +0800

    add stock for daily

--- main/utils/get_dataset.py
+++ main/utils/get_dataset.py
@@ -17,6 +17,49 @@ import json
 
 
 
+def get_data(stock_name='AAPL', start='2010-01-01', end='2015-12-25', interval='1m'):
+    df = yf.download(stock_name, start=start, end=end, group_by='ticker', interval=interval, auto_adjust=True)
+    return df
+
+
+def get_stock_price_info(ticker, period='1y', interval='1d', ):
+    data = yf.download(  # or pdr.get_data_yahoo(...
+        # tickers list or string as well
+        tickers=ticker,
+
+        # use "period" instead of start/end
+        # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+        # (optional, default is '1mo')
+        period=period,
+
+        # fetch data by interval (including intraday if period < 60 days)
+        # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+        # (optional, default is '1d')
+        interval=interval,
+
+        # group by ticker (to access via data['SPY'])
+        # (optional, default is 'column')
+        group_by='ticker',
+
+        # adjust all k line automatically
+        # (optional, default is False)
+        auto_adjust=True,
+
+        # download pre/post regular market hours data
+        # (optional, default is False)
+        prepost=False,
+
+        # use threads for mass downloading? (True/False/Integer)
+        # (optional, default is True)
+        threads=True,
+
+        # proxy URL scheme use use when downloading?
+        # (optional, default is None)
+        proxy=None
+    )
+    return data
+
+
 
 def get_jsonparsed_data(url):
     response = urlopen(url)
--- main/utils/get_finance_data.py
+++ main/utils/get_finance_data.py
@@ -5,7 +5,7 @@ import csv
 import datetime
 import pandas as pd
 import os
-for get
+from get_dateset import get_single_dataset
 try:
     # For Python 3.0 and later
     from urllib.request import urlopen
@@ -14,51 +14,6 @@ except ImportError:
     from urllib2 import urlopen
 
 import json
-
-
-def get_data(stock_name='AAPL', start='2010-01-01', end='2015-12-25', interval='1m'):
-    stock_names = stock_name.split()
-    df = yf.download(stock_name, start=start, end=end, group_by='ticker', interval=interval, auto_adjust=True)
-    return df
-
-
-def get_stock_price_info(ticker, period='1y', interval='1d', ):
-    data = yf.download(  # or pdr.get_data_yahoo(...
-        # tickers list or string as well
-        tickers=ticker,
-
-        # use "period" instead of start/end
-        # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
-        # (optional, default is '1mo')
-        period=period,
-
-        # fetch data by interval (including intraday if period < 60 days)
-        # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
-        # (optional, default is '1d')
-        interval=interval,
-
-        # group by ticker (to access via data['SPY'])
-        # (optional, default is 'column')
-        group_by='ticker',
-
-        # adjust all k line automatically
-        # (optional, default is False)
-        auto_adjust=True,
-
-        # download pre/post regular market hours data
-        # (optional, default is False)
-        prepost=False,
-
-        # use threads for mass downloading? (True/False/Integer)
-        # (optional, default is True)
-        threads=True,
-
-        # proxy URL scheme use use when downloading?
-        # (optional, default is None)
-        proxy=None
-    )
-    return data
-
 
 
 if __name__ == "__main__":
@@ -79,7 +34,7 @@ if __name__ == "__main__":
             print('new directry has been created')
             os.mkdir(save_data + '/' + dataset+ '/' + i)
 
-
+    download_dataset = get_single_dataset(dataset)
     if 'day' in day_data_type:
         intraday = []
         for symbol in download_dataset:
